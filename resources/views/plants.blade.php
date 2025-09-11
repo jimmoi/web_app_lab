c8b709dd79f7d1c27aec1f3c7d22e960994eb753
Nah man, i lose. <selaruang@gmail.com>
2025-09-11 14:51:14 +0700

    Update plants.blade.php
    using css

--- resources/views/plants.blade.php
+++ resources/views/plants.blade.php
@@ -5,47 +5,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        td {
-            text-align : center;
+        td, th {
+            text-align: left;
+            padding: 10px;
         }
 
-        body {
-            font-family : monospace;
-            font-size : 1.25em;
+        .plant_table {
+            font-family: monospace;
+            font-size: 1.25em;
+            border-collapse: collapse;
+        }
+
+        #table_head {
+            border: 2px solid black;
+        }
+
+        .odd {
+            background-color: white;
+        }
+
+        .even {
+            background-color: rgba(226, 226, 226, 1);
         }
     </style>
 </head>
 <body>
-    <h1 style="color:rgba(26, 231, 50, 1); text-decoration:underline;">List of Plants</h1>
-    <table border = 1>
-        <tr>
-            <th>No.</th>
-            <th>Plant Name</th>
-            <th>Scientific Name</th>
-            <th> Garden Name</th>
-        </tr>
-        <?php
-        if (isset($plant_data)) {
-            $count = 1;
-            foreach ($plant_data as $data) {
-                $plant_name = $data->plant_name;
-                $scientific_name = $data->scientific_name;
-                $garden_name = $data->garden->garden_name;
-                echo
-                "<tr>
-                    <td>{$count}</td>
-                    <td>{$plant_name}</td>
-                    <td>{$scientific_name}</td>
-                    <td>{$garden_name}</td>
-                </tr>";
+    <h1 style="color:rgba(26, 231, 50, 1); text-decoration:underline; text-align: center;">List of Plants</h1>
+    <div style="max-width: fit-content; margin-inline: auto;">
+        <table class = "plant_table">
+            <tr id = "table_head">
+                <th>No.</th>
+                <th>Plant Name</th>
+                <th>Scientific Name</th>
+                <th> Garden Name</th>
+            </tr>
+            <?php
+            if (isset($plant_data)) {
+                $count = 1;
+                foreach ($plant_data as $data) {
+                    $plant_name = $data->plant_name;
+                    $scientific_name = $data->scientific_name;
+                    $garden_name = $data->garden->garden_name;
+                    $group = ($count%2 == 0) ? "even":"odd";
+                    echo
+                    "<tr class={$group}>
+                        <td>{$count}</td>
+                        <td>{$plant_name}</td>
+                        <td>{$scientific_name}</td>
+                        <td>{$garden_name}</td>
+                    </tr>";
 
-                $count += 1;
+                    $count += 1;
+                }
+            } else {
+                echo "<tr><td colspan = 4>No data</td></tr>";
             }
-        } else {
-            echo "<tr><td colspan = 4>No data</td></tr>";
-        }
-            
-        ?>
-    </table>
+                
+            ?>
+        </table>
+    </div>
 </body>
 </html>
